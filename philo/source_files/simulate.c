fcd74aa2a84a420068f0674e1f87a72cca348a77
Taneli Valimaki <tvalimak@c2r2p6.hive.fi>
2024-06-11 21:01:13 +0300

    added debug printf's

--- philo/source_files/simulate.c
+++ philo/source_files/simulate.c
@@ -6,7 +6,7 @@
 /*   By: tvalimak <Tvalimak@student.42.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 12:33:50 by tvalimak          #+#    #+#             */
-/*   Updated: 2024/06/11 20:58:34 by tvalimak         ###   ########.fr       */
+/*   Updated: 2024/06/11 21:01:03 by tvalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,6 +95,7 @@ void	*process_simulation(void *param)
 {
 	t_philo_data	*philo;
 
+	printf("Thread created\n");
 	philo = (t_philo_data *)param;
 	if (philo->left_fork == philo->right_fork)
 	{
